9831778f33fac61821cf117613ae9394ce3a90bd
sharathjith7 <82765715+sharathjith7@users.noreply.github.com>
2021-04-25 16:16:17 +0530

    Changes in the indentation
    I have changed the indentation a bit and removed unnecessary functions.

--- Angle Finder.py
+++ Angle Finder.py
@@ -4,6 +4,8 @@ import math
 from PIL import ImageGrab
 
 ####################### SETTINGS ###########################
+
+
 pygame.init()
 
 WIDTH = 555
@@ -19,6 +21,7 @@ WHITE = (255, 255, 255)
 
 FPS = 27
 
+
 ####################### MAIN CLASS ###########################
 
 
@@ -30,15 +33,12 @@ class Main:
         self.mouse_clicked = False
         self.dot_positions = []
         self.mouse_pos = (0, 0)
-        self.m1 = 0  # for dot_positions[0] and dot_positions[1]
-        self.m2 = 0  # for dot_positions[1] and dot_positions[2]
-        self.found_m = False
         self.found_angle = False
         self.angle = 0
         self.clipboard_data = ImageGrab.grabclipboard()
         if self.clipboard_data is not None:
-            self.clipboard_data.save('screen.png')
-            self.clipboard_image = pygame.image.load('screen.png')
+            self.clipboard_data.save('Screen.png')
+            self.clipboard_image = pygame.image.load('Screen.png')
             self.w, self.h = self.clipboard_image.get_width(), self.clipboard_image.get_height()
             self.screen = pygame.display.set_mode((self.w, self.h))
 
@@ -62,16 +62,15 @@ class Main:
                     self.mouse_clicked = True
                     self.dot_positions.append(pygame.mouse.get_pos())
 
-        keys = pygame.key.get_pressed()
-        print(keys)
-        if keys[pygame.KMOD_CTRL]:
-            self.clipboard_data = ImageGrab.grabclipboard()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_v:
+                    self.clipboard_data = ImageGrab.grabclipboard()
 
-            if self.clipboard_data is not None:
-                self.clipboard_data.save('screen.png')
-                self.clipboard_image = pygame.image.load('screen.png')
-                self.w, self.h = self.clipboard_image.get_width(), self.clipboard_image.get_height()
-                self.screen = pygame.display.set_mode((self.w, self.h))
+                    if self.clipboard_data is not None:
+                        self.clipboard_data.save('Screen.png')
+                        self.clipboard_image = pygame.image.load('Screen.png')
+                        self.w, self.h = self.clipboard_image.get_width(), self.clipboard_image.get_height()
+                        self.screen = pygame.display.set_mode((self.w, self.h))
 
         self.mouse_pos = pygame.mouse.get_pos()
 
@@ -79,15 +78,11 @@ class Main:
         if len(self.dot_positions) == 3 and not self.found_angle:
             self.angle = self.find_angle()
             self.found_angle = True
-        '''self.take_pic = pyautogui.screenshot()
-        self.take_pic.save("screen.png")
-        self.display_screen = pygame.image.load('screen.png')'''
 
     def draw(self):
         self.screen.fill(BLACK)
         if self.clipboard_data is not None and self.clipboard_image is not None:
-            #print(self.clipboard_image)
-            self.screen.blit(self.clipboard_image, (0,0))
+            self.screen.blit(self.clipboard_image, (0, 0))
         if self.mouse_clicked:
             if len(self.dot_positions) == 1:
                 pygame.draw.circle(self.screen, YELLOW, self.dot_positions[0], 3)
@@ -104,35 +99,25 @@ class Main:
                 pygame.draw.circle(self.screen, YELLOW, self.dot_positions[1], 3)
                 pygame.draw.line(self.screen, RED, self.dot_positions[1], self.dot_positions[2], 1)
                 pygame.draw.circle(self.screen, YELLOW, self.dot_positions[2], 3)
-                print(self.m1, self.m2)
                 print(self.angle)
-                '''x = self.screen.get_at((100, 100))
-                print(x)'''
 
         pygame.display.update()
-
-    def find_m(self):
-        print(self.dot_positions)
-        self.m1 = abs((self.dot_positions[0][1] - self.dot_positions[1][1]) / (self.dot_positions[0][0] - self.dot_positions[1][0]))
-        self.m2 = abs((self.dot_positions[2][1] - self.dot_positions[1][1]) / (self.dot_positions[2][0] - self.dot_positions[1][0]))
-        self.found_m = True
 
     def find_angle(self):
         x1, y1 = self.dot_positions[0][0], self.dot_positions[0][1]
         x2, y2 = self.dot_positions[1][0], self.dot_positions[1][1]
         x3, y3 = self.dot_positions[2][0], self.dot_positions[2][1]
 
-        angle = math.acos((((x3 - x2) * (x1 - x2)) + ((y3 - y2) * (y1 - y2))) / ((math.sqrt((x3 - x2) ** 2 + (y3 - y2) ** 2)) * (math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2))))
+        angle = math.acos((((x3 - x2) * (x1 - x2)) + ((y3 - y2) * (y1 - y2))) / (
+                (math.sqrt((x3 - x2) ** 2 + (y3 - y2) ** 2)) * (math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2))))
 
         angle = math.degrees(angle)
 
         return angle
 
-# http://www.webmath.com/_answer.php
-# https://www.youtube.com/watch?v=JSEPDJfl8m8
-# https://stackoverflow.com/questions/550001/fully-transparent-windows-in-pygame
 
 ####################### REAL CODE ###########################
+
 
 main = Main()
 main.run()
